54ebba63804873d5be003e21bfde89b024030c95
Codes <hassankhaliq2122@gmail.com>
2026-02-24 16:35:59 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -39,7 +39,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://localhost:3000",
+        
         "https://client-hunter.netlify.app"
     ],
     allow_credentials=True,
